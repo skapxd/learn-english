13deb9d094cb844533dbad1c5434ac962ad44c4e
Manuel <hbiaser132@gmail.com>
2024-04-27 16:20:11 -0500

    Actualizar ruta de generación de contenido en el archivo route.ts

--- app/api/random/google/route.ts
+++ app/api/random/google/route.ts
@@ -1,4 +1,3 @@
-
 import { temas } from "@/utils/const";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
@@ -8,30 +7,28 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
 
 export async function POST(req: Request) {
   try {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     const temaSeleccionado = temas[Math.floor(Math.random() * temas.length)];
+    console.log(temaSeleccionado);
 
-    console.log(temaSeleccionado)
-
-    const prompt = `Genera una oración en ingles completa sobre este tema ${temaSeleccionado}`
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
+    const prompt = `Genera una oración en ingles completa sobre este tema ${temaSeleccionado}`;
+    const text = await generateContent(prompt);
 
     const prompt2 = `Tu trabajo es responder con la oración pero de el modo como se debe pronunciar en ingles pero escrito para que un hispano hablante sepa como pronunciar las palabras, algo similar a lo que hace el Pinyin para el Hanzi, ejemplo si te doy "the sheet is there" tu respondes "de shit is dere" muy similar a como funciona el Pinyi, la idea es hacer esto pero con el ingles para hispano hablantes.
     Las palabras son:
-    "${text}"`
+    "${text}"`;
+    const text2 = await generateContent(prompt2);
 
-    const result2 = await model.generateContent(prompt2);
-    const response2 = await result2.response;
-    const text2 = response2.text();
-
-    console.log(text)
-    return NextResponse.json({ data: text, data2: text2 })
+    console.log(text);
+    return NextResponse.json({ data: text, data2: text2 });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return NextResponse.json(error);
   }
+}
+
+async function generateContent(prompt: string): Promise<string> {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  return response.text();
 }
